ab36072f1d79494d546d6212dd1b3d6ca24a5cb0
zhanarys.kit <zh.sayranbekov@kit.systems>
2020-08-18 23:17:42 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.scss'
 //Components
-import Breadcrumb from './components/Breadcrumb'
 import Header from './components/Header'
 import Product from './components/Product'
 import Spec from './components/Spec.js'
